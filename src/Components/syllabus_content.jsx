65abf93192ff6ea1b897731e9082203cf6dcdbdb
o0PHOENIX0o <pheonixyt5@gmail.com>
2024-09-02 19:12:56 +0530

    Syllabus Page done

--- src/Components/syllabus_content.jsx
+++ src/Components/syllabus_content.jsx
@@ -1,59 +1,59 @@
-
+import React,{useState} from "react";
 import styles from "../assets/css/Syllabus_content.module.css"
+import GetSyllabusData from '../assets/Data/Syllabus.jsx';
 
 const Syllabus_content = function(){
+    const [data,setData] = useState(GetSyllabusData('FCS'));
+
+    const getSylabus = function(event){
+        let element = event.target;
+        const newData = GetSyllabusData(element.innerText);
+        setData(newData);
+        const activeElement = document.querySelector(`.${styles.activeBtn}`);
+        if (activeElement) 
+            activeElement.classList.remove(styles.activeBtn);
+        
+        element.classList.add(styles.activeBtn);
+    }
+
+
     return(
         <div className={styles.container}>
             <ul className={styles.Selectors}>
-                <li className={`${styles.btn}  ${styles.activeBtn}`}>FCS</li>
-                <li className={styles.btn}>DS</li>
-                <li className={styles.btn}>OS</li>
-                <li className={styles.btn}>OOP</li>
-                <li className={styles.btn}>DBMS</li>
-                <li className={styles.btn}>DLCD</li>
-                <li className={styles.btn}>ECO</li>
-                <li className={styles.btn}>C</li>
-                <li className={styles.btn}>Maths</li>
+                <li onClick={getSylabus} className={`${styles.btn}  ${styles.activeBtn}`}>FCS</li>
+                <li onClick={getSylabus} className={styles.btn}>DS</li>
+                <li onClick={getSylabus} className={styles.btn}>OS</li>
+                <li onClick={getSylabus} className={styles.btn}>OOP</li>
+                <li onClick={getSylabus} className={styles.btn}>DBMS</li>
+                <li onClick={getSylabus} className={styles.btn}>DLCD</li>
+                <li onClick={getSylabus} className={styles.btn}>ECO</li>
+                <li onClick={getSylabus} className={styles.btn}>C</li>
+                <li onClick={getSylabus} className={styles.btn}>Maths</li>
             </ul>
             <hr />
 
             <div className={styles.content}>
                 <div className={styles.title}>
-                    <h2>Syllabus FCS</h2>
-                    <h4>ICT 201</h4>
+                    <h2>{data.Subject}</h2>
+                    <h4>{data.SubjectCode}</h4>
                 </div>
                 <div className={styles.subject_container}>
-                    <div className={styles.subject}>
-                        <h3>Unit 1</h3>
+                    {data.syllabus.map((syllabus,index)=>(
+                        <div className={styles.subject} key={index}>
+                        <h3>Unit: {syllabus.unit}</h3>
                         <div className={styles.timeline}>
                             <div className={styles.timeline_item}>
-                                <h4>Title: Sets, Logic, and Relation</h4>
+                                <h4>Title: {syllabus.title}</h4>
                                 <h4>Topics:</h4>
                                 <ul>
-                                    <li>Sets, Subsets, powerset, operations on sets</li>
-                                    <li>Propositional Logic, Rules of inferences in propositional logic</li>
-                                    <li>Quantifiers, Predicates and validity, Predicate Logic, normal forms</li>
-                                    <li>Proof Techniques: Direct Proof, Proof by Contraposition, proof by contradiction</li>
+                                    {syllabus.topics.map((topic,index)=>(
+                                        <li key={index}>{topic}</li>
+                                    ))}
                                 </ul>
                             </div>
                         </div>
                     </div>
-                    
-                    <div className={styles.subject}>
-                        <h3>Unit 1</h3>
-                        <div className={styles.timeline}>
-                            <div className={styles.timeline_item}>
-                                <h4>Title: Sets, Logic, and Relation</h4>
-                                <h4>Topics:</h4>
-                                <ul>
-                                    <li>Sets, Subsets, powerset, operations on sets</li>
-                                    <li>Propositional Logic, Rules of inferences in propositional logic</li>
-                                    <li>Quantifiers, Predicates and validity, Predicate Logic, normal forms</li>
-                                    <li>Proof Techniques: Direct Proof, Proof by Contraposition, proof by contradiction</li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
+                    ))}
                     
                 </div>
 
---
+++ src/assets/Data/Schedule.jsx
@@ -0,0 +1,201 @@
+import React from "react"
+
+
+
+const ScheduleData = [
+    {
+      "Id": "ICT 201",
+      "ShortName": "FCS",
+      "Name": "Foundation of Computer Science",
+      "Credit": "4",
+      "Instructor": "Dr. Rinkaj Goyal",
+      "ClassSchedule": {
+        "Tuesday": {
+          "duration": "2hr",
+          "start": "11:00",
+          "end": "1:00"
+        },
+        "Friday": {
+          "duration": "2hr",
+          "start": "11:00",
+          "end": "1:00"
+        }
+      }
+    },
+    {
+      "id": "ECO 213",
+      "ShortName": "ECO",
+      "Name": "Engineering Economics",
+      "Credit": "2",
+      "Instructor": "TBA",
+      "ClassSchedule": {
+        "Tuesday": {
+          "duration": "2hr",
+          "start": "9:00",
+          "end": "11:00"
+        }
+      }
+    },
+    {
+      "id": "ICT 211",
+      "ShortName": "DS",
+      "Name": "Data Structures",
+      "Credit": "4",
+      "Instructor": "Navin Rajpal",
+      "ClassSchedule": {
+        "Monday": {
+          "duration": "2hr",
+          "start": "11:00",
+          "end": "1:00"
+        },
+        "Thursday": {
+          "duration": "2hr",
+          "start": "11:00",
+          "end": "1:00"
+        }
+      },
+      "LabSchedule": {
+        "Friday": {
+          "duration": "2hr",
+          "start": "3:30",
+          "end": "5:30"
+        }
+      }
+    },
+    {
+      "id": "ICT 209",
+      "ShortName": "OOP",
+      "Name": "Object Oriented Programming Using c++",
+      "Credit": "4",
+      "Instructor": "bhul gaya",
+      "ClassSchedule": {
+        "Tuesday": {
+          "duration": "1hr",
+          "start": "3:30",
+          "end": "4:30"
+        },
+        "Thursday": {
+          "duration": "2hr",
+          "start": "3:30",
+          "end": "5:30"
+        }
+      },
+      "LabSchedule": {
+        "Monday": {
+          "duration": "2hr",
+          "start": "1:30",
+          "end": "3:30"
+        }
+      }
+    },
+    {
+      "id": "ICT 207",
+      "ShortName": "DBMS",
+      "Name": "Database Management System",
+      "Credit": "4",
+      "Instructor": "Prof. Udayan Ghose",
+      "ClassSchedule": {
+        "Thursday": {
+          "duration": "2hr",
+          "start": "1:30",
+          "end": "3:30"
+        },
+        "Friday": {
+          "duration": "2hr",
+          "start": "1:30",
+          "end": "3:30"
+        }
+      },
+      "LabSchedule": {
+        "Monday": {
+          "duration": "2hr",
+          "start": "3:30",
+          "end": "5:30"
+        }
+      }
+    },
+    {
+      "id": "ICT 203",
+      "ShortName": "OS",
+      "Name": "Operating Systems",
+      "Credit": "3",
+      "Instructor": "bhul gaya",
+      "ClassSchedule": {
+        "Tuesday": {
+          "duration": "2hr",
+          "start": "1:30",
+          "end": "3:30"
+        },
+        "Wednesday": {
+          "duration": "2hr",
+          "start": "11:00",
+          "end": "1:00"
+        }
+      },
+      "LabSchedule": {
+        "Wednesday": {
+          "duration": "2hr",
+          "start": "3:30",
+          "end": "5:30"
+        }
+      }
+    },
+    {
+      "id": "ICT 205",
+      "ShortName": "DLCD",
+      "Name": "Digital Logic and Computer Design",
+      "Credit": "4",
+      "Instructor": "TBA",
+      "ClassSchedule": {
+        "Monday": {
+          "duration": "2hr",
+          "start": "9:00",
+          "end": "11:00"
+        },
+        "Wednesday": {
+          "duration": "2hr",
+          "start": "1:30",
+          "end": "3:30"
+        }
+      }
+    },
+    {
+      "id": "BC 123",
+      "ShortName": "C",
+      "Name": "Bridge Course in Programming in C",
+      "Credit": "0",
+      "Instructor": "TBA",
+      "ClassSchedule": {
+        "Wednesday": {
+          "duration": "2hr",
+          "start": "9:00",
+          "end": "11:00"
+        },
+        "Thursday": {
+          "duration": "1hr",
+          "start": "10:00",
+          "end": "11:00"
+        }
+      }
+    },
+    {
+      "id": "BC 121",
+      "ShortName": "Maths",
+      "Name": "Bridge Course in Mathematics",
+      "Credit": "0",
+      "Instructor": "TBA",
+      "ClassSchedule": {
+        "Thursday": {
+          "duration": "2hr",
+          "start": "9:00",
+          "end": "10:00"
+        },
+        "Friday": {
+          "duration": "2hr",
+          "start": "9:00",
+          "end": "11:00"
+        }
+      }
+    }
+];
+
